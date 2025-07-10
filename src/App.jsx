6eae38d28d9117abb3aa2b1f00d909f4e175a81d
ArbereshaA <arbresha.ajrula@gmail.com>
2025-07-10 02:26:33 +0200

    My Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/Arbresha-sPortfolio">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
